26f17d19ffaf7d6c2f1ddf026d4fb8a8dd2e2459
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:13 +0300

    Create aboutUs.js

---
+++ data/aboutUs.js
@@ -0,0 +1,35 @@
+export const infoData = [
+  {
+    title: "Company Overview",
+    description:
+      "IVO Solutions, a Tanzanian company established in 2015, specializes in data center TI services. It offers an array of cloud computing solutions, serving as a one-stop shop for comprehensive data center services.",
+  },
+  {
+    title: "People-Driven Growth",
+    description:
+      "From its inception, IVO Solutions owes its growth to dedicated employees who ensure the delivery of top-tier services. Their commitment plays a pivotal role in providing quality and value to customers.",
+  },
+  {
+    title: "Employee Empowerment",
+    description:
+      "IVO Solutions is committed to hiring, empowering, and retaining exceptional talents. By fostering a united workforce, the company achieves collective goals and strengthens its identity as a respected brand.",
+  },
+  {
+    title: "Building a Strong Brand",
+    description:
+      "IVThrough the collaborative efforts of its employees, IVO Solutions emerges as a reliable and esteemed brand. By aligning the team's efforts, the company establishes itself as a prominent player in the industry.",
+  },
+];
+
+export const principlesData = [
+  {
+    src: "/about/principles_01.webp",
+    header: "Our Vision",
+    text: "To be a successful leading regional information technology company of service excellence that delivers innovative and value drivenICT products and services.",
+  },
+  {
+    src: "/about/principles_02.webp",
+    header: "Our Mission",
+    text: "To be the regional leader in IT Solutions that can bridge the gap between business and technology. We'll achieve this by focusing on innovation, bring new technologies, excel at our competencies and prioritise our customer interests",
+  },
+];
