5f9ac347cbbf5907d412f026dcc36f6e979e40b3
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:19 +0300

    Create loginFormInputs.js

---
+++ data/loginFormInputs.js
@@ -0,0 +1,17 @@
+export const loginFormInputs = [
+  {
+    field: "Your Email",
+    id: "email",
+    type: "text",
+    name: "email",
+    isRequired: false,
+  },
+  {
+    field: "Password",
+    placeholder: "Your Password",
+    id: "password",
+    type: "password",
+    name: "password",
+    isRequired: false,
+  },
+];
