759549c57d5ead5429f91700dd276bc19420cc3a
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:05:01 +0300

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+import ContactHero from "../components/contact/hero";
+import Footer from "../components/common/Footer";
+
+export default function Contact() {
+  return (
+    <main className="body-wrapper">
+      <ContactHero />
+      <Footer />
+    </main>
+  );
+}
