4e6a7730ce8968d2a7fb56b7eb0ec3e56e6c5bee
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:16 +0300

    Create index.js

---
+++ constants/index.js
@@ -0,0 +1,12 @@
+export const NEXT_PUBLIC_PB_URL = "https://pb-ivo.hop.sh";
+export const NEXT_PUBLIC_PB_EMAIL = "developer@dotsyndicate.com";
+export const NEXT_PUBLIC_PB_PASSWORD = "Developer@313";
+
+export const CONTACT_FORM_USER_TEMPLATE_ID = "template_h64n7ra";
+export const CONTACT_FORM_CLIENT_TEMPLATE_ID = "template_5y0sq86";
+
+export const EMAILJS_SERVICE_ID = "service_a94mufr";
+export const EMAILJS_PUBLIC_ID = "nNYRV5JXWmp7oa-48";
+
+export const RECAPTCHA_SITE_KEY = "6LfOOzcpAAAAAKORXQVJxd-Bhhe3jTrA3ewLVQXz";
+export const RECAPTCHA_SECRET_KEY = "6LfOOzcpAAAAAJs5VKm5HufUF2ASzLP65-XwSrdD";
