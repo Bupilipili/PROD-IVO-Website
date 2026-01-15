fca9a765c39a2fa6df80521b75e013086a4d1080
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:26 +0300

    Create serviceSectionData.js

---
+++ data/serviceSectionData.js
@@ -0,0 +1,83 @@
+export const servicesData = [
+  {
+    id: "1",
+    title: "Cloud Services",
+    imageUrl: "/services/services_01.webp",
+    points: [
+      {
+        id: "1",
+        title: "Resilient, Reliable, and Secure",
+        description:
+          "Ivo Solutions offers resilient, reliable, and secure cloud computing services, ensuring uptime and data protection for clients.",
+      },
+      {
+        id: "2",
+        title: "Cost-Effective Managed Cloud Services",
+        description:
+          "Our managed cloud services provide clients with cost-effective operations and expert support throughout the service lifecycle.",
+      },
+      {
+        id: "3",
+        title: "Versatile Cloud Services",
+        description:
+          "Ivo Solutions offers diverse cloud computing services, including private cloud, disaster recovery, and cloud PBX.",
+      },
+    ],
+    bgColor:
+      "linear-gradient(93deg, rgba(255, 205, 7, 0.23) 0%, rgba(255, 205, 7, 0.08) 100%), #FFF",
+  },
+  {
+    id: "2",
+    title: "Managed Services",
+    imageUrl: "/services/services_02.webp",
+    points: [
+      {
+        id: "1",
+        title: "Varied Managed IT Services",
+        description:
+          "Managed IT services encompass a range of offerings, including proactive maintenance and support.",
+      },
+      {
+        id: "2",
+        title: "Cloud-Based Scalability",
+        description:
+          "Cloud-based managed services enable rapid scalability and access to resources.",
+      },
+      {
+        id: "3",
+        title: "Managed vs. Cloud Services",
+        description:
+          "Managed IT services handle both infrastructure and workloads, while cloud services focus on vendor-managed components.",
+      },
+    ],
+    bgColor:
+      "linear-gradient(93deg, rgba(14, 115, 228, 0.11) 0%, rgba(14, 115, 228, 0.03) 100%), #FFF",
+  },
+  {
+    id: "3",
+    title: "IT Consultancy",
+    imageUrl: "/services/services_03.webp",
+    points: [
+      {
+        id: "1",
+        title: "IT Infrastructure Strategy Consulting",
+        description:
+          "Ivo Solutions offers IT infrastructure strategy consultancy services, including server virtualization, to optimize IT management and support.",
+      },
+      {
+        id: "2",
+        title: "Impartial IT Procurement Advice",
+        description:
+          "Ivo Solutions offers IT infrastructure strategy consultancy services, including server virtualization, to optimize IT management and support.",
+      },
+      {
+        id: "3",
+        title: "Cost-Effective Procurement",
+        description:
+          "We assist organizations in navigating IT procurement to achieve cost savings and cost transparency throughout the item's lifespan.",
+      },
+    ],
+    bgColor:
+      "linear-gradient(93deg, rgba(130, 132, 139, 0.27) 0%, rgba(130, 132, 139, 0.15) 100%), #FFF",
+  },
+];
