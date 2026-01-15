2fecafa133983427d59b00651d5c5e4d96b3d3b1
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:43 +0300

    Create testimonialData.js

---
+++ data/testimonialData.js
@@ -0,0 +1,23 @@
+export const testimonials = [
+  {
+    id: "1",
+    name: "Ms. Neema",
+    position: "CEO",
+    company: "Vodacom",
+    text: "Wow! Our team was so impressed with the professional touch that IVO solution technicians used in handling our database errors. Thanks!",
+  },
+  {
+    id: "2",
+    name: "Micheal",
+    position: "HR",
+    company: "PSPF",
+    text: "You guys are the best, With the cloud service your installed last month, I can now easily monitor even when I go on my short business trip.",
+  },
+  {
+    id: "3",
+    name: "Abdul Aziz",
+    position: "MD",
+    company: "Jambo Media",
+    text: "Thanks alot for the timely service. We really liked the security upgrade onto our office network its so efficient and fast",
+  },
+];
