4af9b63a861d74e654a466b80efc6ef9d9090eb4
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:34 +0300

    Create solutionsHome.js

---
+++ data/solutionsHome.js
@@ -0,0 +1,25 @@
+export const solutionsData = [
+  {
+    id: "1",
+    title: "Cloud Solutions",
+    imageUrl: "/solutions/solution_01.webp",
+    text: `<p>All infrastructure, platforms, software, or technologies that users access through the internet without reguiring additional software such as the following;<br/><br/><ul><li>Infrastructure (laaS)</li>
+      <li>Platforms (PaaS)</li><li>Software (Saas)</li><li>Function (FaaS)</li></ul></p>`,
+    redirectUrl: "/solutions/cloud-solutions",
+  },
+  {
+    id: "2",
+    title: "Infrastructure Solutions",
+    imageUrl: "/solutions/solution_02.webp",
+    text: `<p>Managed IT services can range from general to specific, depending on your needs.
+      Common services can include the monitoring and maintenance of equipment, TI systems management, remote monitoring and management of servers, network monitoring, a n d other support services.</p>`,
+    redirectUrl: "/solutions/infrastructure-solutions",
+  },
+  {
+    id: "3",
+    title: "Network Solutions",
+    imageUrl: "/solutions/solution_03.webp",
+    text: `<p>Our comprehensive services encompass infrastructure solutions such as communication, networking, data management, and more, tailored to each business's needs. Additionally, we specialize in network security, establishing a secure environment for devices, applications, and users to operate safely and confidently.</p>`,
+    redirectUrl: "/solutions/network-solutions",
+  },
+];
