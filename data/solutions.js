5c195a686e0cafd19da6adbce89470f3ecd52d3e
Erick Clement Bupilipili <bupepperleather@gmail.com>
2026-01-15 20:06:23 +0300

    Create solutions.js

---
+++ data/solutions.js
@@ -0,0 +1,183 @@
+export const solutionsData = [
+  {
+    id: "1",
+    name: "cloud-solutions",
+    title: "Cloud Solutions",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>All infrastructure, platforms, software, or technologies that users access through the internet without requiring additional software downloads can be considered cloud computing services-including the following as-a-Service solutions.<br/><br/>Clouds are TI environments that abstract, pool, and share scalable resources across a network. Clouds enable cloud computing, which is the act of running workloads within a cloud environment. Clouds are a type of Paas, because hardware and an application software platform is provided by another party.<br/><br/>IVO SOLUTIONS services include hosted and managed platform, application, and data services that accelerate time to value and reduce the operational cost and complexity of delivering cloud-native applications. The core of IVO SOLUTIONS cloud services are availability and effectiveness of application platforms from major cloud providers. Organizations can confidently build and scale applications with a streamlined experience across services and hybrid cloud environments while IVO SOLUTIONS manages the rest.</p>`,
+      },
+      {
+        id: "2",
+        title: "Infrastructure-as-a-Service (Iaas)",
+        description:
+          "<p>Infrastructure-as-a-Service (laaS) provides users with compute, networking, and storage resources.</p>",
+      },
+      {
+        id: "3",
+        title: "Platforms-as-a-Service (Paas)",
+        description:
+          "<p>Platforms-as-a-Service (PaaS) provides users with a platform on which applications can run, as well as all the TI infrastructure required for it to run.</p>",
+      },
+      {
+        id: "4",
+        title: "Software-as-a-Service (Saas)",
+        description:
+          "<p>Software-as-a-Service (Saas) provides users with-essentially-a cloud application, the platform on which it runs, and the platform's underlying infrastructure.</p>",
+      },
+      {
+        id: "5",
+        title: "Function-as-a-Service (Faas)",
+        description:
+          "<p>Function-as-a-Service (FaaS), an event-driven execution model, lets developers build, run, and manage app packages as functions without maintaining the infrastructure.</p>",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "infrastructure-solutions",
+    title: "Infrastructure Solutions",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>Infrastructure services include communication services, networking, data processing and storage, platforms through which businesses can share content and media,
+        knowledge management, systems, applications, IoT, user devices, resilience. Most importantly, infrastructure services provide a help desk to support end users with a
+        Service Level Agreement (SLA) that guarantees a level of service.<br/><br/>IVO SOLUTIONS provides an end-to-end infrastructure service that is affordable, scalable, secure, flexible and reliable.<br/><br/>We help clients drive performance, quickly adapt to changing market dynamics, and achieve their digital transformation goals by leveraging emerging technologies along
+        with a range of services—including infrastructure consultancy, technology advisory services, service desk, storage as a service, infrastructure rationalization and
+        virtualization, and more.</p>`,
+      },
+      {
+        id: "2",
+        title: "Why IT infrastructure is important",
+        description: `<p>Technology powers nearly every aspect of today's businesses, from an individual employee's work to operations to goods and services. When properly networked,
+technology can be optimized to improve communication, create efficiencies and increase productivity.<br/><br/>If an IT infrastructure is flexible, reliable and secure, it can help an enterprise meet its goals and provide a competitive edge in the market. Alternatively, if an IT infrastructure
+isn't properly implemented, businesses can face connectivity, productivity and security issues—like system disruptions and breaches. Overall, having a properly implemented
+infrastructure can be a factor in whether a business is profitable or not.</p>`,
+      },
+      {
+        id: "3",
+        title: "With an IT infrastructure, a company can",
+        description: `<br/><ul><li>Provide a positive customer experience by providing uninterrupted access to its website and online store.</li><li>Develop and launch solutions to market with speed</li>
+        <li>Collect data in real time to make quick decisions</li><li>Improve employee productivity</li>`,
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "network-solutions",
+    title: "Network Solutions",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>Enterprise networks provide the connectivity essential for every organization. But, with the onset of hybrid IT and cloud solutions, the enterprise network is more critical and more complex than ever.</p>`,
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "aws",
+    title: "AWS",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>We provide cloud consulting services that help you migrate, optimize, and manage applications on the AWS platform. We help you leverage the full potential of the cloud to streamline and automate your business processes, reduce operational costs, and gain a competitive edge.</p>`,
+      },
+      {
+        id: "2",
+        title: "",
+        description: `<p>Transform your business with the cloud trusted by top brands like Netflix, Adobe and the BBC. AWS delivers, on average, 31% infrastructure cost savings* and 62% more efficient IT infrastructure management. Our experienced team is here to help you get the most out of your cloud investment. Get started today!</p>`,
+      },
+    ],
+  },
+  {
+    id: "5",
+    name: "cyber security",
+    title: "Cyber Security",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>Our Cyber Security Consultation Service is dedicated to protecting your business systems from cybercriminals. We understand that a comprehensive defence strategy is critical, and that's why we combine our extensive cybersecurity expertise with cutting-edge resources to shield your data, applications, networks, and cloud infrastructure from attackers. </p>`,
+      },
+      {
+        id: "2",
+        title: "Consulting Services ",
+        description:
+          "<p>Work with our consultants to proactively secure your networking technology, from the design phase through to installation, deployment, and optimisation. </p>",
+      },
+      {
+        id: "3",
+        title:
+          "Planning And Implementation | Configuration and Protection | On-site and Remote Assistance",
+        description:
+          "<p>we are committed to providing you with the best network security solutions to help you protect your business, your clients, and your valuable data. Contact us today to learn more about our services and how we can help you secure your network</p>",
+      },
+    ],
+  },
+  {
+    id: "6",
+    name: "azure",
+    title: "Azure",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>We take pride in being a leading Managed Services and IT Solutions Provider in Tanzania. With a commitment to excellence and innovation, we have strategically partnered with Microsoft to bring cutting-edge Azure and Microsoft services to the Tanzanian market.</p>`,
+      },
+      {
+        id: "2",
+        title: "",
+        description: `<p>This partnership enables us to deliver world-class Azure services tailored to meet the unique needs of businesses and organizations in Tanzania. As an authorized Azure services provider, we bring you the power of Microsoft's cloud technology right to your doorstep.
+        </p>`,
+      },
+    ],
+  },
+  {
+    id: "7",
+    name: "azure cloud solutions",
+    title: "Azure Cloud Solutions",
+    textSections: [
+      {
+        id: "1",
+        title: "",
+        description: `<p>At IVO Solutions (T) LTD, we prioritize security as a key aspect to be considered. With Azure security solutions, we offer a comprehensive range of services designed to protect your digital assets from potential threats. Partnering with Microsoft, we bring you cutting-edge security tools and technologies to ensure the highest level of data protection. Azure security solutions include: </p>`,
+      },
+      {
+        id: "2",
+        title: "Azure Sentinel ",
+        description:
+          "<p>Harness the power of Azure Sentinel, a cloud-native security information and event management (SIEM) tool that uses advanced analytics and AI to detect and respond to threats in real-time. Gain deep visibility into your environment and proactively mitigate security risks. </p>",
+      },
+      {
+        id: "3",
+        title:
+          "Azure Active Directory (Azure AD) Identity Protection / Microsoft Entra ID",
+        description:
+          "<p>Safeguard your digital identities with Azure AD Identity Protection. Detect and prevent identity-based attacks, such as account compromise and brute force attacks, by leveraging advanced threat intelligence and adaptive policies.</p>",
+      },
+      {
+        id: "4",
+        title: "Azure Information Protection",
+        description:
+          "<p>Protect your sensitive data with Azure Information Protection. Classify, label, and encrypt your documents and emails to ensure data confidentiality and prevent unauthorized access. Maintain control over your data even when it is shared externally.</p>",
+      },
+      {
+        id: "5",
+        title: "Azure Security Center",
+        description:
+          "<p>Gain a centralized view of your security posture with Azure Security Center. Monitor and manage the security of your Azure resources, identify vulnerabilities, and apply security recommendations to strengthen your overall security posture.</p>",
+      },
+      {
+        id: "6",
+        title: "Azure Intune",
+        description:
+          "<p>Manage and secure your organization's devices with Azure Intune. Enforce compliance policies, remotely manage devices, and protect corporate data on both company-owned and employee-owned devices.</p>",
+      },
+    ],
+  },
+];
